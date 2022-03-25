592394ff6da1a2d09dd1e9d7c781b488ac5b389c
drhosseinjavedani <h.javedani@gmail.com>
2022-03-25 13:17:53 -0700

    test ci/cd

---
+++ main.py
@@ -0,0 +1,10 @@
+import xgboost
+
+def print_main():
+    print('this is main')
+    return True
+
+
+if __name__=='__main__':
+    print_main()
+
--- processor/add_numbers.py
+++ processor/add_numbers.py
@@ -1,6 +1,6 @@
 import xgboost as xgb
 def add_two_number(a,b):
     print('add two numbers:', a+b)
-    print(xgb.__name__)
     return a+b
+
 
