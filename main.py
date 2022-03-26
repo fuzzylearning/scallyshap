4a57d6614c0dbdf20661d5d084f1e90838c2bab3
drhosseinjavedani <h.javedani@gmail.com>
2022-03-25 18:03:05 -0700

    test ci/cd

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from feature_selector.scally_feature_selector import ScallyFeatureSelector
 def print_main():
     print('this is main')
     SFC = ScallyFeatureSelector()
-    data = pd.read_csv('data/default of credit card clients.csv')
+    data = pd.read_csv('data/data.csv')
     print(data.head())
     print(data.columns.to_list())
     X = data.loc[:,data.columns!='default payment next month']
